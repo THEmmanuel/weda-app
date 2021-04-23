b729bc1f6b3b0f5d45d89ac7b6404d254efafe23
Emmanuel Ayodele <emmayodayo@gmail.com>
2021-04-23 23:35:32 +0100

    initial commit

---
+++ android/app/src/main/java/com/weda_app_ts/MainActivity.java
@@ -0,0 +1,15 @@
+package com.weda_app_ts;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "weda_app_ts";
+  }
+}
