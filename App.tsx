a0aff4d061c6d50e6078935cc2f3e7b53bbf3800
Emmanuel Ayodele <emmayodayo@gmail.com>
2021-04-26 12:20:30 +0100

    Added colors! (and dummy component stuff?)

--- App.tsx
+++ App.tsx
@@ -1,12 +1,55 @@
 /* eslint-disable prettier/prettier */
+// import axios from 'axios';
 
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {
+  // useState,
+  // useEffect,
+} from 'react';
+
+import {
+  View,
+  Text,
+  // Image,
+} from 'react-native';
+
+
+// const BASE_URL = ' https://api.weatherbit.io/v2.0/current?';
+// const API_KEY = 'db0049048589475e9e668c253e679e28';
 
 const App = () => {
+  // const [weatherData, setWeatherData] = useState('');
+  // const [cityName, setCityName] = useState('');
+
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       setCityName('Ibadan');
+  //       const response = await axios(`${BASE_URL}&city=${cityName}&key=${API_KEY}`);
+  //       console.log(`${BASE_URL}&city=${cityName}&key=${API_KEY}`);
+  //       setWeatherData(response.data.timezone);
+  //       console.log(response.data);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, [cityName]);
+
+
   return (
     <View>
-      <Text>Stuff</Text>
+      <View>
+        <Text>Location Icon</Text>
+        <Text>Ciy name</Text>
+        <Text>Calendar icon</Text>
+      </View>
+
+      <View>
+        <Text>Icon? bg?</Text>
+        <Text>current weather: eg 25 deg c</Text>
+        <Text>components containing the date and all.</Text>
+      </View>
     </View>);
 };
 
@@ -14,7 +57,13 @@ export default App;
 
 
 //Todos
+//Get API calls set-up
 //Add mainweather component thingy
 //Add the silder thingy?
-
+//Build out the UI, fill it with dummy data
 //Routes to weekly view
+
+// setCityName('Ibadan');
+      // const response = await axios(`${BASE_URL}&city=${cityName}&key=${API_KEY}`);
+      // console.log(`${BASE_URL}&city=${cityName}&key=${API_KEY}`);
+      // setWeatherData(response.data.timezone);
---
+++ constants/Colors.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+
+export default {
+    uiColors: {
+        textBlue: '#2D2C5E',
+        bgBlue: '#72EFED',
+        cardBlue: '#95F6F5',
+        darkBlue: '#232634',
+        text: '#F0F0F0',
+    },
+};
